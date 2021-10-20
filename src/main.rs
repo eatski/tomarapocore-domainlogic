d424fabc38c4f4ec503ec08fe481f9bd8538ae19
hagaitski <i.haga629@gmail.com>
2021-10-21 05:30:14 +0900

    fix: blank

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use exprocess::core::{ExprocessCore};
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct AppCore;
+impl ExprocessCore for AppCore {
+    type State = AppState;
+
+    type Command = AppCommand;
+
+    type Result = AppResult;
+
+    fn init() -> Self::State {
+        AppState::Blank
+    }
+
+    fn resolve(prev: &Self::State, command: &Self::Command) -> Self::Result {
+        todo!()
+    }
+
+    fn reducer(prev: &Self::State, result: &Self::Result) -> Self::State {
+        todo!()
+    }
+}
+
+
+enum AppState {
+    Blank
+}
+enum AppCommand {
+
+}
+
+enum AppResult {
+
 }
