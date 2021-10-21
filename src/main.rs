c2567796db601ed9374813bf82e09adde02e386f
hagaitski <i.haga629@gmail.com>
2021-10-21 13:58:47 +0900

    fix: ドメイン

--- src/main.rs
+++ src/main.rs
@@ -36,3 +36,63 @@ enum AppCommand {
 enum AppResult {
 
 }
+
+type CharacterId = u32;
+
+struct Character {
+    id:CharacterId,
+    cls: CharacterClass
+}
+
+/**
+ * クラス
+ */
+enum CharacterClass {
+    Ordinary(ordinary::Ordinary) /* 一般人 */,
+    Brainwashed /* 洗脳された人 */,
+    Insane /* 発狂した人 */
+}
+
+pub mod ordinary {
+    use crate::{CharacterId, ItemId};
+    pub struct Ordinary {
+        directivities: Vec<Directivity>,
+        helth: Helth
+    }
+
+    /**
+        指向性 そのキャラの価値観などが反映される
+    */
+    pub enum Directivity {
+        Alive /* 生存 */,
+        Possession(ItemId) /* アイテムなどの所持 */,
+        Love(CharacterId) /* 対象キャラクターが生存しているかどうか */,
+        Hate(CharacterId) /* 対象キャラクターが死亡しているかどうか */
+    }
+
+    enum Helth {
+        Alive(HelthAlive),
+        Dead
+    }
+
+    type Sanity = u32;
+    
+    struct HelthAlive {
+        sanity: Sanity
+    }
+}
+
+type ItemId = u32;
+
+/**
+ * 
+ */
+struct Item {
+    id: ItemId,
+    cls: ItemClass
+}
+
+enum ItemClass {
+    Book,
+    Other
+}
